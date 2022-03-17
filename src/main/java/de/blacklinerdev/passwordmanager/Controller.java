9d95823b82d60e0e4fdfee48921ebd356ff94619
Orbitan <sarah97hehn@outlook.de>
2022-03-17 20:34:43 +0100

    model and controller finnished

--- src/main/java/de/blacklinerdev/passwordmanager/Controller.java
+++ src/main/java/de/blacklinerdev/passwordmanager/Controller.java
@@ -45,7 +45,7 @@ public class Controller implements ActionListener, Observer {
         }
         return true;
     }
-    
+  // test
     @Override
     public void actionPerformed(ActionEvent ae) {
 
